00b5304dfef332f4c308619b94cac51df5917445
MUHUMUZAJeanPierre <j.muhumuza@alustudent.com>
2024-10-25 13:28:03 +0200

    feat/feat

--- resident_management/settings.py
+++ resident_management/settings.py
@@ -183,7 +183,7 @@ WSGI_APPLICATION = 'resident_management.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'residentialmanagement',
         'USER': 'postgres',
         'PASSWORD': '12345',
