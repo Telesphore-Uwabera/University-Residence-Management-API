366b5a6fbeaf2eb928182282cf6994e6243d1cdc
TelesphoreUwabera <t.uwabera@alustudent.com>
2024-10-31 18:13:54 +0200

    Update settings.py

--- residence/settings.py
+++ residence/settings.py
@@ -151,8 +151,7 @@ WSGI_APPLICATION = 'resident_management.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',  # Path to your SQLite database file
-    }
+        'NAME': BASE_DIR / 'db.sqlite3',  
 }
 
 # Password validation
